3b80eebc8dc4f74c57fd4d95e90dc5ead5a1ab02
blitznihar <blitznihar@gmail.com>
2018-12-20 22:21:49 -0600

    initial draft

---
+++ src/main/java/com/blitz/springboot/basics/GetStarted/GetStartedApplication.java
@@ -0,0 +1,14 @@
+package com.blitz.springboot.basics.GetStarted;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GetStartedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GetStartedApplication.class, args);
+	}
+
+}
+
